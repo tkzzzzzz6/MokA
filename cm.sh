ec8758a096ff2a3b7913869bbc75912c73ce4ffd
tkzzzzzz6 <tanke@stu.sicau.edu.cn>
2026-04-09 01:45:22 +0800

    在 cm.sh 中添加更新和安装依赖的命令，以支持数据集下载和环境配置

--- cm.sh
+++ cm.sh
@@ -3,3 +3,13 @@ uv sync --no-cache --index-url https://pypi.tuna.tsinghua.edu.cn/simple
 .\scripts\pretrain\prepare_audiocaps_500_100_windows.ps1 -CookiesFromBrowser chrome
 
 wget.exe -c https://huggingface.co/datasets/LanguageBind/Video-LLaVA/resolve/main/llava_image.zip
+
+apt update
+apt install -y aria2
+
+export HF_ENDPOINT=https://hf-mirror.com
+
+aria2c -x 16 -s 16 -c \
+https://huggingface.co/datasets/LanguageBind/Video-LLaVA/resolve/main/llava_image.zip
+
+wget -c https://hf-mirror.com/datasets/LanguageBind/Video-LLaVA/resolve/main/llava_image.zip
